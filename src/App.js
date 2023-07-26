e6b12511eaf89f8139edb68d0f8705b70b2ad80a
Iefan <66840141+realiefan@users.noreply.github.com>
2023-07-26 16:41:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -94,9 +94,8 @@ const App = () => {
       {!buttonClicked && (
         <div style={{ position: 'relative', marginBottom: '2rem' }}>
           <div>
-            <h1 className="text-4xl font-bold mt-4 px-5 mb-2">ℕostrNet.work</h1>
-             <h2 className="text-sm font-bold mx-8 mb-4 px-5">One PWA to organize all Nostr WebApps and other PWAs
-              & use any website as a Native App. (Mobile only)</h2>
+            <h1 className="text-4xl font-bold mt-4 px-5 mb-2">ℕ</h1>
+             <h2 className="text-sm font-bold mx-8 mb-4 px-5">ℕostrNetwork, It offers a unified dashboard to manage all your Nostr web apps in one place. (Beta)</h2>
           </div>
           <div style={{ position: 'fixed', right: '5%', bottom: '0' }}>
             <button className="px-4 py-2 text-sm rounded font-bold text-white" onClick={handleDeleteAllClick}>
