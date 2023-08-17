9f8c9c59646296412ef1fa3b68a723ea38eca520
Iefan <66840141+realiefan@users.noreply.github.com>
2023-08-18 01:09:50 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -276,7 +276,7 @@ const closeTextModal = () => {
           )}
         
           <button
-                className="absolute right-8 px-1 py-1 mr-2  text-xs font-semibold bg-gray-600 rounded-full font-mono text-gray-200 hover:bg-gray-100 hover:text-gray-900"
+                className="absolute right-1 px-1 py-1 mr-2  text-xs font-semibold bg-gray-600 rounded-full font-mono text-gray-200 hover:bg-gray-100 hover:text-gray-900"
                 onClick={() => openTextModal("This is the text modal content.")}
               >
                 <svg width="15px" height="15px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -291,38 +291,7 @@ const closeTextModal = () => {
        
        
 
-          <div className="left-0 ">
-            <button
-              type="button"
-              onClick={toggleDropdown2}
-              className=" absolute bg-[#303479]  right-corner-container px-1 py-1 text-xs top-0 rounded-full font-semibold text-gray-200"
-            >
-              
-              <svg width="15px" height="15px" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" strokeWidth="3" stroke="#000000" fill="none"><circle cx="34.52" cy="11.43" r="5.82"/><circle cx="53.63" cy="31.6" r="5.82"/><circle cx="34.52" cy="50.57" r="5.82"/><circle cx="15.16" cy="42.03" r="5.82"/><circle cx="15.16" cy="19.27" r="5.82"/><circle cx="34.51" cy="29.27" r="4.7"/><line x1="20.17" y1="16.3" x2="28.9" y2="12.93"/><line x1="38.6" y1="15.59" x2="49.48" y2="27.52"/><line x1="50.07" y1="36.2" x2="38.67" y2="46.49"/><line x1="18.36" y1="24.13" x2="30.91" y2="46.01"/><line x1="20.31" y1="44.74" x2="28.7" y2="48.63"/><line x1="17.34" y1="36.63" x2="31.37" y2="16.32"/><line x1="20.52" y1="21.55" x2="30.34" y2="27.1"/><line x1="39.22" y1="29.8" x2="47.81" y2="30.45"/><line x1="34.51" y1="33.98" x2="34.52" y2="44.74"/></svg>
-            </button>
-            {isDropdown2Open && (
-              <div className="absolute right-0 mr-2 mt-2 w-32 text-left  rounded-md shadow-lg z-10 bg-[#353237] ring-opacity-5">
-                <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
-                  <a
-                    href="https://labs.perplexity.ai/"
-                    className="block px-4 py-2 text-xs font-semibold rounded-md font-mono text-gray-200 hover:bg-gray-100 hover:text-gray-900"
-                    role="menuitem"
-                    rel="noopener noreferrer"
-                  >
-                    AI Chat
-                  </a>
-                  <a
-                    href="https://www.perplexity.ai/"
-                    className="block px-4 py-2 text-xs font-semibold rounded-md font-mono text-gray-200 hover:bg-gray-100 hover:text-gray-900"
-                    role="menuitem"
-                    rel="noopener noreferrer"
-                  >
-                    AI Search
-                  </a>
-                </div>
-              </div>
-            )}
-          </div>
+          
           
         </div>
         
