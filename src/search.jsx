510068323728360e5f3452a51b25555fda1ec3c5
Iefan <66840141+realiefan@users.noreply.github.com>
2023-08-26 03:06:26 +0530

    Update search.jsx

--- src/search.jsx
+++ src/search.jsx
@@ -41,34 +41,34 @@ const DuckDuckGoSearchBar = () => {
     setExtModelOpen(false);
   };
 
-  return (
-    <div className="flex items-center justify-center">
-      <SearchForm
-        value={searchQuery}
-        onChange={setSearchQuery}
-        onSubmit={handleSearchSubmit}
-        placeholder="Search on Nostr"
-      />
+    return (
+      <div className="flex items-center justify-center">
+        <SearchForm
+          value={searchQuery}
+          onChange={setSearchQuery}
+          onSubmit={handleSearchSubmit}
+          placeholder="Search on Nostr"
+        />
 
-      <div className="relative inline-block px-1 ">
-        <div className="bg-[#303479] shadow-lg text-semibold  rounded-full">
-          <div className="" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
-            <button
-              onClick={() => openModal(<iframe src="https://labs.perplexity.ai/" title="AI Chat" className="w-full h-full bg-[#18181a] rounded-xl" />)}
-              className="block px-1 py-1 text-xs font-semibold rounded-full font-mono text-gray-200 hover:bg-gray-100 hover:text-gray-900"
-              role="menuitem"
-            >
-              <svg width="20px" height="20px" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" strokeWidth="3" stroke="#000000" fill="none"><circle cx="34.52" cy="11.43" r="5.82"/><circle cx="53.63" cy="31.6" r="5.82"/><circle cx="34.52" cy="50.57" r="5.82"/><circle cx="15.16" cy="42.03" r="5.82"/><circle cx="15.16" cy="19.27" r="5.82"/><circle cx="34.51" cy="29.27" r="4.7"/><line x1="20.17" y1="16.3" x2="28.9" y2="12.93"/><line x1="38.6" y1="15.59" x2="49.48" y2="27.52"/><line x1="50.07" y1="36.2" x2="38.67" y2="46.49"/><line x1="18.36" y1="24.13" x2="30.91" y2="46.01"/><line x1="20.31" y1="44.74" x2="28.7" y2="48.63"/><line x1="17.34" y1="36.63" x2="31.37" y2="16.32"/><line x1="20.52" y1="21.55" x2="30.34" y2="27.1"/><line x1="39.22" y1="29.8" x2="47.81" y2="30.45"/><line x1="34.51" y1="33.98" x2="34.52" y2="44.74"/></svg>
-            </button>
+        <div className="relative inline-block px-1 ">
+          <div className="bg-[#303479] shadow-lg text-semibold  rounded-full">
+            <div className="" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+              <button
+                onClick={() => openModal(<iframe src="https://labs.perplexity.ai/" title="AI Chat" className="w-full h-full rounded-xl" />)}
+                className="block px-1 py-1 text-xs font-semibold rounded-full font-mono text-gray-200 hover:bg-gray-100 hover:text-gray-900"
+                role="menuitem"
+              >
+                <svg width="20px" height="20px" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" strokeWidth="3" stroke="#000000" fill="none"><circle cx="34.52" cy="11.43" r="5.82"/><circle cx="53.63" cy="31.6" r="5.82"/><circle cx="34.52" cy="50.57" r="5.82"/><circle cx="15.16" cy="42.03" r="5.82"/><circle cx="15.16" cy="19.27" r="5.82"/><circle cx="34.51" cy="29.27" r="4.7"/><line x1="20.17" y1="16.3" x2="28.9" y2="12.93"/><line x1="38.6" y1="15.59" x2="49.48" y2="27.52"/><line x1="50.07" y1="36.2" x2="38.67" y2="46.49"/><line x1="18.36" y1="24.13" x2="30.91" y2="46.01"/><line x1="20.31" y1="44.74" x2="28.7" y2="48.63"/><line x1="17.34" y1="36.63" x2="31.37" y2="16.32"/><line x1="20.52" y1="21.55" x2="30.34" y2="27.1"/><line x1="39.22" y1="29.8" x2="47.81" y2="30.45"/><line x1="34.51" y1="33.98" x2="34.52" y2="44.74"/></svg>
+              </button>
+              
+
+            </div>
             
-
           </div>
           
         </div>
-        
-      </div>
-      <button
-              className="bg-[#303479] p-1 shadow-lg text-semibold rounded-full"
+        <button
+        className="bg-[#303479] p-1 shadow-lg text-semibold rounded-full"
               onClick={openExtModel}
               
             >
