961fed2b5cccc41816aae2eac8fb2bf10ff52222
Iefan <66840141+realiefan@users.noreply.github.com>
2023-09-02 09:54:11 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -300,12 +300,7 @@ const closeTextModal = () => {
                       </svg>
 
               </button>
-              <button
-              className="absolute right-1 px-1 py-1 mr-10  text-xs font-semibold bg-gray-600 rounded-full font-mono text-gray-200 hover:bg-gray-100 hover:text-gray-900"
-              onClick={openExtModel}
-            >
-              <svg fill="#000000" width="15px" height="15px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M19 10V7c0-1.103-.897-2-2-2h-3c0-1.654-1.346-3-3-3S8 3.346 8 5H5c-1.103 0-2 .897-2 2v4h1a2 2 0 0 1 0 4H3v4c0 1.103.897 2 2 2h4v-1a2 2 0 0 1 4 0v1h4c1.103 0 2-.897 2-2v-3c1.654 0 3-1.346 3-3s-1.346-3-3-3z"/></svg>
-            </button>
+              
               
         </div>
         
