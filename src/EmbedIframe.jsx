470f5517e41a7584e7d1dfdeb9fe522025b45187
Iefan <66840141+realiefan@users.noreply.github.com>
2023-09-04 14:23:24 +0530

    Create EmbedIframe.jsx

---
+++ src/EmbedIframe.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const EmbedIframe = ({ url, title }) => {
+  return (
+    <iframe
+      src={url}
+      frameBorder="0"
+      scrolling="yes"
+      className="embed-iframe max-h-full"
+      title={title}
+      allow="clipboard-write; autoplay; camera; microphone; geolocation; fullscreen"
+      allowFullScreen
+      loading="lazy"
+      sandbox="allow-same-origin allow-scripts allow-popups allow-forms"
+    />
+  );
+};
+
+export default EmbedIframe;
