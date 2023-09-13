a3f93a3cb6c3514f062e1183cde1993edbda6cbc
Iefan <66840141+realiefan@users.noreply.github.com>
2023-09-13 06:02:37 +0530

    Update EmbedIframe.jsx

--- src/EmbedIframe.jsx
+++ src/EmbedIframe.jsx
@@ -8,9 +8,9 @@ const EmbedIframe = ({ url, title }) => {
       scrolling="yes"
       className="embed-iframe max-h-full"
       title={title}
-      allow="accelerometer; autoplay; camera; clipboard-write; encrypted-media; fullscreen; geolocation; gyroscope; microphone; midi; payment; picture-in-picture"
+      allow="accelerometer; autoplay; camera; clipboard-write; encrypted-media; fullscreen; geolocation; gyroscope; microphone; midi; payment; picture-in-picture; downloads" // Add 'downloads' to the list of allowed permissions
       loading="lazy"
-      sandbox=" allow-scripts allow-same-origin allow-popups allow-forms"
+      sandbox="allow-scripts allow-same-origin allow-popups allow-forms allow-downloads" // Add 'allow-downloads' here
     />
   );
 };
