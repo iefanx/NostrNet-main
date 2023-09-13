c0f2227beb945b259733a22d4dfd62e845d3686b
Iefan <66840141+realiefan@users.noreply.github.com>
2023-09-13 06:08:22 +0530

    Update EmbedIframe.jsx

--- src/EmbedIframe.jsx
+++ src/EmbedIframe.jsx
@@ -3,39 +3,15 @@ import React from 'react';
 const EmbedIframe = ({ url, title }) => {
   return (
     <iframe
-  src={url}
-  frameBorder="0"
-  scrolling="yes"
-  className="embed-iframe max-h-full"
-  title={title}
-  allow="
-    accelerometer;
-    autoplay;
-    camera;
-    clipboard-write;
-    encrypted-media;
-    fullscreen;
-    geolocation;
-    gyroscope;
-    microphone;
-    midi;
-    payment;
-    picture-in-picture;
-    downloads;
-  "
-  loading="lazy"
-  sandbox="
-    allow-scripts;
-    allow-same-origin;
-    allow-popups;
-    allow-forms;
-    allow-modals;
-    allow-orientation-lock;
-    allow-pointer-lock;
-    allow-top-navigation;
-    allow-top-navigation-by-user-activation;
-  "
-/>
+      src={url}
+      frameBorder="0"
+      scrolling="yes"
+      className="embed-iframe max-h-full"
+      title={title}
+      allow="accelerometer; autoplay; camera; clipboard-write; encrypted-media; fullscreen; geolocation; gyroscope; microphone; midi; payment; picture-in-picture; downloads" // Add 'downloads' to the list of allowed permissions
+      loading="lazy"
+      sandbox="allow-scripts allow-same-origin allow-popups allow-forms allow-downloads" // Add 'allow-downloads' here
+    />
   );
 };
 
